ce9fd66a4dbbb7a8ef3ff6cee7e795f4cb9ed018
Daurius Swarts <ali@openmindi.co.za>
2022-10-28 23:52:53 +0200

    Create Update

--- pages/create.tsx
+++ pages/create.tsx
@@ -1,13 +1,17 @@
 import Head from 'next/head'
-import React from 'react'
+import React, { FormEvent, useState } from 'react'
 import Header from '../components/Header'
-import { useAddress, useContract, useOwnedNFTs } from '@thirdweb-dev/react'
+import { MediaRenderer, useAddress, useContract, useCreateAuctionListing, useCreateDirectListing, useNetwork, useNetworkMismatch, useOwnedNFTs } from '@thirdweb-dev/react'
+import { NATIVE_TOKEN_ADDRESS, NFT } from '@thirdweb-dev/sdk'
+import network from '../utils/network'
+import { useRouter } from 'next/router'
 
 
 type Props = {}
 
 function Create({}: Props) {
     const address = useAddress();
+    const router = useRouter();
     const { contract } = useContract(
         process.env.NEXT_PUBLIC_MARKETPLACE_CONTRACT,
         "marketplace"
@@ -20,6 +24,67 @@ function Create({}: Props) {
 
         const ownedNfts = useOwnedNFTs(collectionContract, address);
 
+        const [selectedNft, setSelectedNft] = useState<NFT>();
+
+        const networkMismatch = useNetworkMismatch();
+        const [, switchNetwork] = useNetwork();
+
+        const { mutate: createDirectListing, isLoading, error} = useCreateDirectListing(contract);
+        const { mutate: createAuctionListing, isLoading: isLoadingAuction, error: errorAuction} = useCreateAuctionListing(contract);
+
+        const handleCreateListing = async (e: FormEvent<HTMLFormElement>) => {
+            e.preventDefault();
+
+            if (networkMismatch) {
+                switchNetwork && switchNetwork(network);
+                return;
+            }
+            if (!selectedNft) return;
+                const target = e.target as typeof e.target & {
+                elements: {listingType:{ value: string }; price: { value: string }};
+         
+            };
+            const { listingType, price } = target.elements;
+
+            if (listingType.value === "directListing") {
+                createDirectListing({
+                    assetContractAddress: process.env.NEXT_PUBLIC_COLLECTION_CONTRACT!,
+                    tokenId: selectedNft.metadata.id,
+                    currencyContractAddress: NATIVE_TOKEN_ADDRESS,
+                    listingDurationInSeconds: 60 * 60 * 24 * 7, // 1 week
+                    quantity: 1,
+                    buyoutPricePerToken: price.value,
+                    startTimestamp: new Date()
+                }, {
+                    onSuccess(data, variables, context) {
+                        router.push("/");
+                    },
+                    onError(error, variables, context) {
+                        console.log("Error: ", error, variables, context)
+                    },
+                })
+            }
+
+            if (listingType.value === "auctionListing") {
+                createAuctionListing({
+                    assetContractAddress: process.env.NEXT_PUBLIC_COLLECTION_CONTRACT!,
+                    buyoutPricePerToken: price.value,
+                    tokenId: selectedNft.metadata.id,
+                    startTimestamp: new Date(),
+                    currencyContractAddress: NATIVE_TOKEN_ADDRESS,
+                    listingDurationInSeconds: 60 * 60 * 24 * 7, // 1 week
+                    quantity: 1,
+                    reservePricePerToken: 0,
+                }, {
+                    onSuccess(data, variables, context) {
+                        router.push("/");
+                    },
+                    onError(error, variables, context) {
+                        console.log("Error: ", error, variables, context)
+                    },
+            });
+        }
+        };
 
   return (
     <div>
@@ -34,9 +99,47 @@ function Create({}: Props) {
             <h2 className='text-xl font-semibold pt-5'>Select an item to sell</h2>
             <hr className='mb-5' />
             <p className='text-sm text-gray-500'>Below you will find the NFT's you own in your wallet</p>
+        
+            <div className='flex overflow-x-scroll space-x-2 p-4'>
+                {ownedNfts?.data?.map((nft) => (
+                    <div key={nft.metadata.image}
+                    onClick={() => setSelectedNft(nft)}
+                    className={`flex flex-col space-y-2 card min-w-fit 
+                    border-2 bg-gray-100 ${nft.metadata.id === selectedNft?.metadata.id
+                    ? "border-black"
+                    : "border-transparent"}`}>
+                    <MediaRenderer className='h-48 rounded-lg'
+                    src={nft.metadata.image}/>
+                    <p className='text-lg truncate font-bold'>{nft.metadata.name}</p>
+                    <p className='text-sm truncate'>{nft.metadata.description}</p> 
+                </div>
+                ))}
+               
+            </div>
+
+            {selectedNft && (
+                <form onSubmit={handleCreateListing}>
+                    <div className='flex flex-col p-10'>
+                    <div className='grid grid-cols-2 gap-5'>
+                        <label className='border-r font-light'>Direct / Fixed Price</label>
+                        <input type="radio" name='listingType' value="directlisting" className='ml-auto h-10 w-10'/>
+                        
+                        <label className='border-r font-light'>Auction</label>
+                        <input type="radio" name='listingType' value="auctionlisting" className='ml-auto h-10 w-10'/>
+                    
+                        <label className='border-r font-light'>Price</label>
+                        <input type="text" name='price' placeholder='0.05' className='bg-gray-100 p-5' />
+                    </div>
+                    <button type='submit'
+                    className='bg-black text-white font-semibold w-56 md:mt-5 mx-auto md:ml-auto rounded-lg p-4 mt-8'>
+                        Create Listing
+                    </button>
+                    </div>
+                </form>
+            )}
         </main>
     </div>
   )
-}
+};
 
 export default Create
